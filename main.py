7b4b3de0f83c2aef1494e319952595ba11d30e8d
D1Sykes <peder.bakk1@gmail.com>
2024-04-21 19:10:07 +0200

    Improvements and such

--- main.py
+++ main.py
@@ -1,68 +1,25 @@
+import settings
 import discord
-import os
-
 from discord.ext import commands
-from dotenv import load_dotenv
-intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='!', intents=intents)
 
-@bot.event
-async def on_ready():
-    print('Vi er live')
+badwords = ['cs', 'cs2']
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    if 'cs' in message.content.lower():
-        await message.channel.send('Nei.')
+def run():
+    intents = discord.Intents.all()
+    bot = commands.Bot(command_prefix='!', intents=intents)
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    if 'counterstrike' in message.content.lower():
-        await message.channel.send('Nei.')
+    @bot.event
+    async def on_ready():
+        print(f'{bot.user} er live')
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    if 'cs2' in message.content.lower():
-        await message.channel.send('Nei.')
+    @bot.event
+    async def on_message(message):
+        for i in badwords:
+            if i in message.content.lower():
+                await message.channel.send('Nei.')
+            return
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    if 'counter strike 2' in message.content.lower():
-        await message.channel.send('Nei.')
+    bot.run(settings.token)
 
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    if 'counterstrike2' in message.content.lower():
-        await message.channel.send('Nei.')
-
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    if 'counter strike' in message.content.lower():
-        await message.channel.send('Nei.')
-
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    if 'homsesex' in message.content.lower():
-        await message.channel.send('Homsesex er deilig ass')
-
-@bot.command()
-async def hei(ctx):
-    await ctx.send('hade')
-
-load_dotenv()
-token=os.getenv('DISCORD_TOKEN')
-bot.run(token)
+if __name__ == "__main__":
+    run()
