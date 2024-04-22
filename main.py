c694784ba341c3b1ce1771ca464206d9981011b9
D1Sykes <peder.bakk1@gmail.com>
2024-04-22 21:19:48 +0200

    Some larger improvents, added functionality

--- main.py
+++ main.py
@@ -15,10 +15,17 @@ def run():
 
     @bot.event
     async def on_message(message):
+        if message.author == bot.user:
+            return
         for i in badwords:
             if i in message.content.lower():
                 await message.channel.send('Nei.')
-            return
+        await bot.process_commands(message)  
+
+    @bot.command(name="ec")
+    async def ecCalc(ctx, num : float):
+        result = num * 6.3
+        await ctx.send(f"{num} Empire Coins er ish {result} norske kroner ;-)")
 
     bot.run(settings.token)
 
