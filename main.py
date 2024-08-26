9d14149d5767c9bb85e8f6386f3975320fe3c733
D1Sykes <peder.bakk1@gmail.com>
2024-08-26 21:28:32 +0200

    Bugfixezzzz

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-# Import needed libraries
-
 import settings
 import discord
 import random
-
 from discord.ext import commands
 
-badwords = ['cs', 'cs2','counter strike'] # Custom variable used in later function(s)
+badwords = ['cs', 'cs2', 'counter strike'] 
 badwords_responses = ['Nei.', 'Hvilken del av nei forstår du ikke?', 'Ikke faen', 'Uaktuelt.', 'Legg deg.']
 
 def run(): # Main program
@@ -15,29 +12,27 @@ def run(): # Main program
     intents = discord.Intents.all()
     bot = commands.Bot(command_prefix='!', intents=intents)
 
-
-    # Prints a message to console if launched succesfully
+    # Prints a message to console if launched successfully
     @bot.event 
     async def on_ready():
         print(f'{bot.user} er live')
 
-
-    # Simple event which listens for the aformentioned "badwords" and prints a response if detected
+    # Simple event which listens for the aforementioned "badwords" and prints a response if detected
     @bot.event
     async def on_message(message):
-        if message.author == bot.user: # This prevents the bot from going in a loop looking for the word, I don't really know why.
+        if message.author == bot.user:  # Prevents the bot from responding to itself
             return
         for i in badwords:
             if i in message.content.lower():
                 random_response = random.sample(badwords_responses, k=1)
                 for response in random_response:
                     await message.channel.send(response)
-            await bot.process_commands(message)  # This line allows the bot to do other things while the event is going, again keeping it from being stuck in a loop. Also, I don't know why.
-
+                break  # Exit loop after the first bad word is found and responded to
+        await bot.process_commands(message)  # Process  other commands after checking for bad words
 
     # Simple multiplication calc for CSGOEmpire coins to Norwegian Kroner conversion
     @bot.command(name="ec")
-    async def ecCalc(ctx, num : float):
+    async def ecCalc(ctx, num: float):
         result = num * 6.3
         await ctx.send(f"{num} Empire Coins er ish {result} norske kroner ;-)")
 
